e3c2c48c37aa0db6ff5b25362bc8a5ecdf8d76a4
MerienMoelker <113899800+MerienMoelker@users.noreply.github.com>
2022-09-20 09:30:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,16 +3,34 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
-gullit = 'Ruud Gullit '
-van_basten = 'Marco van Basten '
+
+scorer_0 = 'Ruud Gullit'
+scorer_1 = 'Marco van Basten'
+
 goal_0 = 32
 goal_1 = 54
-scorers = gullit + str(goal_0) +', ' + van_basten + str(goal_1)
-report = f'{gullit}scored in the {goal_0}nd minute\n{van_basten}scored in the {goal_1}th minute'
+
+scorers = scorer_0 + ' ' + str(goal_0) +', ' + scorer_1 + ' ' + str(goal_1)
+
+report = f'{scorer_0} scored in the {goal_0}nd minute\n{scorer_1} scored in the {goal_1}th minute'
 print(report)
+
+
 player = 'Adri van Tiggelen'
+
 first_name = player[0:player.find(' ')]
-last_name_len = len(player[player.find('v'):])
+
+last_name_len = len(player[player.find(' '):-1])
+
 name_short = player[0] + '.' + player[player.find(' '):]
+
 chant = ((first_name + "! ") * len(first_name))[0:-1]
-good_chant = chant != ' '
+
+good_chant = chant[-1] != ' '
+
+print(player)
+print(first_name)
+print(last_name_len)
+print(name_short)
+print(chant)
+print(good_chant)
